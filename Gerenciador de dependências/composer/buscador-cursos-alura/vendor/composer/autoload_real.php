731c0081f249cb106337850deefb1df46fd1bfc4
Ruan Narici <ruannarici@hotmail.com>
2023-09-10 10:09:24 -0300

    Gerenciador de dependencias

--- Gerenciador de dependências/composer/buscador-cursos-alura/vendor/composer/autoload_real.php
+++ Gerenciador de dependências/composer/buscador-cursos-alura/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInited37a8eacf2c96f01ebb342eabf59e41
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInited37a8eacf2c96f01ebb342eabf59e41', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInited37a8eacf2c96f01ebb342eabf59e41', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInited37a8eacf2c96f01ebb342eabf59e41
         call_user_func(\Composer\Autoload\ComposerStaticInited37a8eacf2c96f01ebb342eabf59e41::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInited37a8eacf2c96f01ebb342eabf59e41::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
