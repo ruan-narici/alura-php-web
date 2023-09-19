21eed527e8eeb1d7906406efbc9427de3937ce22
Ruan Narici <ruannarici@hotmail.com>
2023-09-19 16:17:26 -0300

    Trabalhando com Banco de Dados

--- Trabalhando com Banco de Dados/curso-pdo/remover-aluno.php
+++ Trabalhando com Banco de Dados/curso-pdo/remover-aluno.php
@@ -1,5 +1,21 @@
 <?php
 
+use ruannarici\Pdo\Domain\Model\Student;
 
+require_once 'vendor/autoload.php';
+
+$databasePath = __DIR__ . '/BancoDeDados.sqlite';
+
+$pdo = new PDO('sqlite:' . $databasePath);
+
+$sqlDelete = "
+DELETE FROM students 
+    WHERE id = :id
+";
+
+$prepareStatement = $pdo->prepare($sqlDelete);
+$prepareStatement->bindValue(':id', 2, PDO::PARAM_INT);
+
+$prepareStatement->execute();
 
 ?>
