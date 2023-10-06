8da4ef2e57856c7eba060067c16bd9e9b4145510
Ruan Narici <ruannarici@hotmail.com>
2023-10-05 22:42:27 -0300

    PHP na Web

--- PHP na Web/serenatoCafe/index.php
+++ PHP na Web/serenatoCafe/index.php
@@ -1,57 +1,68 @@
 <?php
-    $arrayCafes = [
-        [
-            "nome" => "Café Cremoso",
-            "descricao" => "Café cremoso irresistivelmente suave e que envolve seu paladar",
-            "valor" => "5.00",
-            "imagem" => "img/cafe-cremoso.jpg"
-        ],
-        [
-            "nome" => "Café com Leite",
-            "descricao" => "A harmonia perfeita do café e do leite, uma experiência reconfortante",
-            "valor" => "2.00",
-            "imagem" => "img/cafe-com-leite.jpg"
-        ],
-        [
-            "nome" => "Cappuccino",
-            "descricao" => "Café suave, leite cremoso e uma pitada de sabor adocicado",
-            "valor" => "7.00",
-            "imagem" => "img/cappuccino.jpg"
-        ],
-        [
-            "nome" => "Café Gelado",
-            "descricao" => "Café gelado refrescante, adoçado e com notas sutis de baunilha ou caramelo",
-            "valor" => "3.00",
-            "imagem" => "img/cafe-gelado.jpg"
-        ]
-    ];
 
-    $arrayAlmoco = [
-        [
-            "nome" => "Bife",
-            "descricao" => "Bife, arroz com feijão e uma deliciosa batata frita",
-            "valor" => "27.90",
-            "imagem" => "img/bife.jpg"
-        ],
-        [
-            "nome" => "Filé de peixe",
-            "descricao" => "Filé de peixe salmão assado, arroz, feijão verde e tomate",
-            "valor" => "24.99",
-            "imagem" => "img/prato-peixe.jpg"
-        ],
-        [
-            "nome" => "Frango",
-            "descricao" => "Saboroso frango à milanesa com batatas fritas, salada de repolho e molho picante",
-            "valor" => "23.00",
-            "imagem" => "img/prato-frango.jpg"
-        ],
-        [
-            "nome" => "Fettuccine",
-            "descricao" => "Prato italiano autêntico da massa do fettuccine com peito de frango grelhado",
-            "valor" => "22.50",
-            "imagem" => "img/fettuccine.jpg"
-        ]
-    ];
+    require_once './src/Connection.php';
+
+    $sql1 = "SELECT * FROM produtos WHERE tipo = 'Café'";
+    $statement = $pdo->query($sql1);
+    $arrayCafes = $statement->fetchAll();
+
+    $sql2 = "SELECT * FROM produtos WHERE tipo = 'Almoço'";
+    $statement = $pdo->query($sql2);
+    $arrayAlmoco = $statement->fetchAll();
+
+    // $arrayCafes = [
+    //     [
+    //         "nome" => "Café Cremoso",
+    //         "descricao" => "Café cremoso irresistivelmente suave e que envolve seu paladar",
+    //         "valor" => "5.00",
+    //         "imagem" => "img/cafe-cremoso.jpg"
+    //     ],
+    //     [
+    //         "nome" => "Café com Leite",
+    //         "descricao" => "A harmonia perfeita do café e do leite, uma experiência reconfortante",
+    //         "valor" => "2.00",
+    //         "imagem" => "img/cafe-com-leite.jpg"
+    //     ],
+    //     [
+    //         "nome" => "Cappuccino",
+    //         "descricao" => "Café suave, leite cremoso e uma pitada de sabor adocicado",
+    //         "valor" => "7.00",
+    //         "imagem" => "img/cappuccino.jpg"
+    //     ],
+    //     [
+    //         "nome" => "Café Gelado",
+    //         "descricao" => "Café gelado refrescante, adoçado e com notas sutis de baunilha ou caramelo",
+    //         "valor" => "3.00",
+    //         "imagem" => "img/cafe-gelado.jpg"
+    //     ]
+    // ];
+
+    // $arrayAlmoco = [
+    //     [
+    //         "nome" => "Bife",
+    //         "descricao" => "Bife, arroz com feijão e uma deliciosa batata frita",
+    //         "valor" => "27.90",
+    //         "imagem" => "img/bife.jpg"
+    //     ],
+    //     [
+    //         "nome" => "Filé de peixe",
+    //         "descricao" => "Filé de peixe salmão assado, arroz, feijão verde e tomate",
+    //         "valor" => "24.99",
+    //         "imagem" => "img/prato-peixe.jpg"
+    //     ],
+    //     [
+    //         "nome" => "Frango",
+    //         "descricao" => "Saboroso frango à milanesa com batatas fritas, salada de repolho e molho picante",
+    //         "valor" => "23.00",
+    //         "imagem" => "img/prato-frango.jpg"
+    //     ],
+    //     [
+    //         "nome" => "Fettuccine",
+    //         "descricao" => "Prato italiano autêntico da massa do fettuccine com peito de frango grelhado",
+    //         "valor" => "22.50",
+    //         "imagem" => "img/fettuccine.jpg"
+    //     ]
+    // ];
 
 ?>
 
@@ -89,11 +100,11 @@
                 <?php foreach($arrayCafes as $cafe): ?>
                 <div class="container-produto">
                     <div class="container-foto">
-                        <img src="<?= $cafe["imagem"]?>">
+                        <img src="./img/<?= $cafe["imagem"]?>">
                     </div>
                     <p><?= $cafe["nome"]?></p>
                     <p><?= $cafe["descricao"]?></p>
-                    <p><?= "R$ " . $cafe["valor"]?></p>
+                    <p><?= "R$ " . $cafe["preco"]?></p>
                 </div>
                 <?php endforeach; ?>
                 
@@ -108,11 +119,11 @@
                 <?php foreach($arrayAlmoco as $almoco): ?>
                 <div class="container-produto">
                     <div class="container-foto">
-                        <img src="<?= $almoco['imagem']?>">
+                        <img src="./img/<?= $almoco['imagem']?>">
                     </div>
                     <p><?= $almoco['nome'] ?></p>
                     <p><?= $almoco['descricao'] ?></p>
-                    <p><?= "R$ " . $almoco['valor'] ?></p>
+                    <p><?= "R$ " . $almoco['preco'] ?></p>
                 </div>
                 <?php endforeach; ?>
             </div>
