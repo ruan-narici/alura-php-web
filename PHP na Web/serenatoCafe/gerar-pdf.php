45a46c864ba3c79266fc56c05643a00b52553d81
Ruan Narici <ruannarici@hotmail.com>
2023-10-15 16:39:29 -0300

    PHP na Web

--- PHP na Web/serenatoCafe/gerar-pdf.php
+++ PHP na Web/serenatoCafe/gerar-pdf.php
@@ -1,16 +1,19 @@
 <?php
 
 require_once "vendor/autoload.php";
+ob_start();
+require_once "./conteudo-pdf.php";
+$html = ob_get_clean();
 
 // reference the Dompdf namespace
 use Dompdf\Dompdf;
 
 // instantiate and use the dompdf class
 $dompdf = new Dompdf();
-$dompdf->loadHtml('hello world');
+$dompdf->loadHtml($html);
 
 // (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
+$dompdf->setPaper('A4');
 
 // Render the HTML as PDF
 $dompdf->render();
