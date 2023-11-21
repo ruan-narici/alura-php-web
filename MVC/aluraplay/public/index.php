4c7f217a30366f243490182d07c04dc6c4092247
Ruan Narici <ruannarici@hotmail.com>
2023-11-20 22:14:31 -0300

    MVC

--- MVC/aluraplay/public/index.php
+++ MVC/aluraplay/public/index.php
@@ -3,21 +3,23 @@
 declare(strict_types= 1);
 
 if (!array_key_exists("PATH_INFO", $_SERVER) || $_SERVER['PATH_INFO'] === "/") {
-    require_once "listar-video.php";
+    require_once __DIR__ . "/../listar-video.php";
 } else if ($_SERVER['PATH_INFO'] === "/novo-video") {
     if ($_SERVER['REQUEST_METHOD'] === "GET") {
-        require_once "./pages/enviar-video.html";
+        require_once __DIR__ . "/../pages/enviar-video.html";
     } else if ($_SERVER['REQUEST_METHOD'] === "POST") {
-        require_once "./novo-video.php";
+        require_once __DIR__ . "/../novo-video.php";
     }
 } else if ($_SERVER['PATH_INFO'] === "/editar-video") {
     if ($_SERVER['REQUEST_METHOD'] === "GET") {
-        require_once "./formulario.php";
+        require_once __DIR__ . "/../formulario.php";
     } else if ($_SERVER['REQUEST_METHOD'] === "POST") {
-        require_once "./editar-video.php";
+        require_once __DIR__ . "/../editar-video.php";
     }
 } else if ($_SERVER['PATH_INFO'] === "/remover-video") {
-    require_once "./remover-video.php";
+    require_once __DIR__ . "/../remover-video.php";
+} else {
+    http_response_code(404);
 }
 
 ?>
