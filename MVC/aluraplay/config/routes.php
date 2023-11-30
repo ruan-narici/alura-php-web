f9c2814fe72b09a2cd1f525990a2c72117acce50
Ruan Narici <ruannarici@hotmail.com>
2023-11-29 23:44:47 -0300

    MVC

---
+++ MVC/aluraplay/config/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+use Alura\Mvc\Controller\VideoForm;
+use Alura\Mvc\Controller\VideoFormUpdate;
+use Alura\Mvc\Controller\VideoList;
+use Alura\Mvc\Controller\VideoNew;
+use Alura\Mvc\Controller\VideoRemove;
+use Alura\Mvc\Controller\VideoUpdate;
+
+$routes = [
+    "GET|/" => VideoList::class,
+    "GET|/novo-video" => VideoForm::class,
+    "POST|/novo-video" => VideoNew::class,
+    "GET|/editar-video" => VideoFormUpdate::class,
+    "POST|/editar-video" => VideoUpdate::class,
+    "GET|/remover-video" => VideoRemove::class
+
+];
+
+?>
--- MVC/aluraplay/public/index.php
+++ MVC/aluraplay/public/index.php
@@ -2,16 +2,10 @@
 
 declare(strict_types= 1);
 
-use Alura\Mvc\Controller\VideoError404;
-use Alura\Mvc\Controller\VideoList;
-use Alura\Mvc\Controller\VideoForm;
-use Alura\Mvc\Controller\VideoFormUpdate;
-use Alura\Mvc\Controller\VideoNew;
 use Alura\Mvc\Repository\VideoRepository;
-use Alura\Mvc\Controller\VideoRemove;
-use Alura\Mvc\Controller\VideoUpdate;
 
 require_once __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . "/../config/routes.php";
 
 $HOST = "localhost";
 $DBNAME = "alura_play";
@@ -21,25 +15,12 @@ $conn = new PDO("mysql:host=$HOST;dbname=$DBNAME", $DBUSER, $DBPASS);
 
 $videoRepository = new VideoRepository($conn);
 
-if (!array_key_exists("PATH_INFO", $_SERVER) || $_SERVER['PATH_INFO'] === "/") {
-    $controller = new VideoList($videoRepository);
-} else if ($_SERVER['PATH_INFO'] === "/novo-video") {
-    if ($_SERVER['REQUEST_METHOD'] === "GET") {
-        $controller = new VideoForm();
-    } else if ($_SERVER['REQUEST_METHOD'] === "POST") {
-        $controller = new VideoNew($videoRepository);
-    }
-} else if ($_SERVER['PATH_INFO'] === "/editar-video") {
-    if ($_SERVER['REQUEST_METHOD'] === "GET") {
-        $controller = new VideoFormUpdate($videoRepository);
-    } else if ($_SERVER['REQUEST_METHOD'] === "POST") {
-        $controller = new VideoUpdate($videoRepository);
-    }
-} else if ($_SERVER['PATH_INFO'] === "/remover-video") {
-    $controller = new VideoRemove($videoRepository);
-} else {
-    $controller = new VideoError404();
-}
+$pathInfo = $_SERVER['PATH_INFO'] ?? "/";
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+$routeClass = $routes[$requestMethod . "|" . $pathInfo];
+
+$controller = new $routeClass($videoRepository);
 
 $controller->dataProcess();
 
